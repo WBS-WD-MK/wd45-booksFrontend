bf371380057bed598f541fb4ba2e859cf906282e
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2024-01-04 11:22:33 +0300

    base structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <div>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+};
+
+export default Main;
